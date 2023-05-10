40298c97e945eb8acd2f7977c069c38f296f348a
zhenchenglin <zhl132@ucsd.edu>
2023-05-10 14:56:06 -0700

    added java classes

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,77 +1,32 @@
 import java.io.IOException;
 import java.net.URI;
-import java.util.ArrayList;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    ArrayList<String> s = new ArrayList<>();
-    ArrayList<String> sLab = new ArrayList<>();
+    int num = 0;
 
-    public String handleRequest(URI url) 
-    {
-        if (url.getPath().equals("/")) 
-        {
-            String words = "";
-            if(sLab.size() == 0)
-            {
-                return String.format("Nothing added yet.");
-            }
-            for(String word : sLab)
-            {
-                words += "\n" + word ;
-            }
-            return String.format(words);
-        } 
-        else if (url.getPath().equals("/add")) 
-        {
-            if (url.getQuery().contains("s")) 
-            {
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Zhencheng's number: %d", num);
+        } else if (url.getPath().equals("/increment")) {
+            num += 1;
+            return String.format("Number incremented!");
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("s"))
-                 {
-                    s.add(parameters[1]);
-                    return "Added.";
+                if (parameters[0].equals("count")) {
+                    num += Integer.parseInt(parameters[1]);
+                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
                 }
             }
-            return "add?";
-        }
-        else if (url.getPath().equals("/search")) 
-        {
-            String words = "Added words are listed below: \n";
-            if(s.size() != 0)
-            {
-                for(String word : s)
-                {
-                    words += "\n" + word ;
-                }
-                return String.format(words);
-            }
-            return "Not added anything yet";
-        }
-        else if (url.getPath().equals("/add-message")) 
-        {
-            if (url.getQuery().contains("sLab")) 
-            {
-                String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("sLab"))
-                 {
-                    sLab.add(parameters[1]);
-                    return "Added.";
-                }
-            }
-            return "add?";
-        }
-         else 
-        {
             return "404 Not Found!";
         }
     }
 }
 
-
-public class SearchEngine 
-{
+class StringServer {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
@@ -80,6 +35,6 @@ public class SearchEngine
 
         int port = Integer.parseInt(args[0]);
 
-        StringServer.start(port, new Handler());
+        Server.start(port, new Handler());
     }
 }
--- StringServer.java
+++ StringServer.java
@@ -73,13 +73,13 @@ class Handler implements URLHandler {
 public class StringServer 
 {
     public static void main(String[] args) throws IOException {
-        if(args.length == 0){
-            System.out.println("Missing port number! Try any number between 1024 to 49151");
-            return;
-        }
+    if(args.length == 0){
+        System.out.println("Missing port number! Try any number between 1024 to 49151");
+        return;
+    }
 
-        int port = Integer.parseInt(args[0]);
+    int port = Integer.parseInt(args[0]);
 
-        StringServer.start(port, new Handler());
+    Server.start(port, new Handler());
     }
 }
